336c2383d8bab0212aaa37a26a62891b89edd0b8
Edward Buckler <esb33@cornell.edu>
2020-05-18 16:55:51 -0400

    Updated build and gradlew

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,22 +21,22 @@ dependencies {
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
-    implementation("org.jetbrains.bio:bioinf-commons:0.0.9")
+//    implementation("org.jetbrains.bio:bioinf-commons:0.0.9")
     implementation("com.github.samtools:htsjdk:2.21.3")
     implementation("de.mpicbg.scicomp:krangl:0.11")
     implementation("net.maizegenetics:tassel:5.2.60")
     implementation("org.jetbrains.kotlin:kotlin-script-runtime:1.3.71")
-    implementation("org.biojava:biojava:5.3.0")
-    implementation("org.biojava:biojava-genome:5.3.0")
+//    implementation("org.biojava:biojava:5.3.0")
+//    implementation("org.biojava:biojava-genome:5.3.0")
     implementation("org.graalvm.sdk:graal-sdk:20.0.0")
     implementation("org.apache.commons:commons-csv:1.8")
-    implementation("com.github.jkcclemens:khttp:0.1.0")
+//    implementation("com.github.jkcclemens:khttp:0.1.0")
 
-    implementation("ai.djl:api:0.5.0")
-    runtimeOnly( "ai.djl.tensorflow:tensorflow-native-auto:2.1.0")
-    runtimeOnly("ai.djl.tensorflow:tensorflow-model-zoo:0.5.0")
-    runtimeOnly("ai.djl.pytorch:pytorch-model-zoo:0.5.0")
-    runtimeOnly("ai.djl.pytorch:pytorch-native-auto:1.5.0")
+//    implementation("ai.djl:api:0.5.0")
+//    runtimeOnly( "ai.djl.tensorflow:tensorflow-native-auto:2.1.0")
+//    runtimeOnly("ai.djl.tensorflow:tensorflow-model-zoo:0.5.0")
+//    runtimeOnly("ai.djl.pytorch:pytorch-model-zoo:0.5.0")
+//    runtimeOnly("ai.djl.pytorch:pytorch-native-auto:1.5.0")
 
 }
 
@@ -57,7 +57,7 @@ tasks {
         outputFormat = "html"
         outputDirectory = "$buildDir/dokka"
         configuration {
-            includes = listOf("/Users/edbuckler/Code/appliedcentraldogma/src/main/kotlin/biokotlin/packages.md")
+            includes = listOf("/Users/edbuckler/Code/biokotlin/src/main/kotlin/biokotlin/packages.md")
         }
     }
 }
