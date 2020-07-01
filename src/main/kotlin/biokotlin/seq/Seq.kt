b4e5b3c15c1c5cf456ff6c11a35386c122c5a2ff
Edward Buckler <esb33@cornell.edu>
2020-07-01 12:47:01 -0400

    Removed an unnecessary map statement

--- src/main/kotlin/biokotlin/seq/Seq.kt
+++ src/main/kotlin/biokotlin/seq/Seq.kt
@@ -114,7 +114,7 @@ internal fun compatibleBioSet(seq: String): List<BioSet> {
                 val origCharBits = bytePresent.clone() as BitSet
                 origCharBits.andNot(it.bitSets)
                 origCharBits.cardinality() == 0
-            }.map { it }
+            }
     if (compatibleSets.isEmpty()) throw IllegalStateException("The characters in the String are not compatible with RNA, DNA, or AminoAcids. " +
             "Or they are a mix of RNA and DNA")
     return compatibleSets
