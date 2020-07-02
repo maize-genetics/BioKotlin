3e0f39218511c4d113ce3752bae3a997491e657e
Vaishnavi Gupta <vg222@cornell.edu>
2020-07-02 13:31:51 -0400

    made nucseqrecord and proteinseqrecord

--- src/main/kotlin/biokotlin/seq/SeqRecord.kt
+++ src/main/kotlin/biokotlin/seq/SeqRecord.kt
@@ -12,14 +12,33 @@ Additional attributes:
 - description - Additional text (string)
 
  */
-class SeqRecord(seq: Seq, id: String, name: String? = null, description: String? = null) {
+interface SeqRecord {
+    val id: String;
+    val name: String?;
+    val description: String?;
+    fun len() : Int;
+    operator fun get(i: IntRange): Seq;
+}
+
+class NucSeqRecord(seq: NucSeq, id: String, name: String? = null, description: String? = null) :
+        SeqRecord {
+    override val id = id;
+    override val name = name;
+    override val description = name;
     val sequence = seq;
-    val id = id;
-    val name = name;
-    val description = name;
+    operator fun get(i: Int): NUC = sequence[i];
+    override operator fun get(i: IntRange): NucSeq = sequence[i];
+    override fun len() : Int = sequence.len();
+}
 
-    fun len(): Int {
-        return sequence.len();
-    }
-
+class ProteinSeqRecord(seq: ProteinSeq, id: String, name: String? = null, description: String? =
+        null) :
+        SeqRecord {
+    val sequence = seq;
+    override val id = id;
+    override val name = name;
+    override val description = name;
+    operator fun get(i: Int): AminoAcid = sequence[i];
+    override operator fun get(i: IntRange): ProteinSeq = sequence[i];
+    override fun len() : Int = sequence.len();
 }
