60c92ea22bb3f37160105259558a3e22dd81360d
Vaishnavi Gupta <vg222@cornell.edu>
2020-07-13 09:08:12 -0400

    added delegation for seqrecords

--- src/main/kotlin/biokotlin/seq/SeqRecord.kt
+++ src/main/kotlin/biokotlin/seq/SeqRecord.kt
@@ -1,4 +1,5 @@
 @file:JvmName("SeqRecord")
+
 package biokotlin.seq
 
 /**
@@ -16,31 +17,21 @@ Additional attributes:
 >>> val record_1 = SeqRecord(Seq("ATCG"), "1", "seq1", "the first sequence")
 
  */
-sealed class SeqRecord(val id: String, val name: String?, val description: String?) {
+sealed class SeqRecord(val id: String, val name: String?, val description: String?,
+                       annotations: Map<String, String>?) {
 
+    val annotations = annotations?.toMap();
     /** Returns the length of the sequence in the record.*/
-    abstract fun len() : Int
+    abstract fun len(): Int
 
-    /** Returns a subset [Seq] of the sequence in the record based on the [IntRange] given.
-     *
-     * Kotlin range operator is "..". Indices start at zero.
-     * Note Kotlin [IntRange]s are inclusive end, while Python slices exclusive end.
-     * Negative slices "-3..-1" start from the last base (i.e. would return the last three bases).
-     */
-    abstract operator fun get(i: IntRange): Seq
 }
 
 class NucSeqRecord(val sequence: NucSeq, id: String, name: String? = null, description: String? =
-        null) :
-        SeqRecord(id, name, description) {
-    operator fun get(i: Int): NUC = sequence[i]
-    override operator fun get(i: IntRange): NucSeq = sequence[i]
-    override fun len() : Int = sequence.len()
+        null, annotations: Map<String, String>? = null) :
+        SeqRecord(id, name, description, annotations), NucSeq by sequence {
 }
 
 class ProteinSeqRecord(val sequence: ProteinSeq, id: String, name: String? = null,
-                       description: String? = null) : SeqRecord(id, name, description) {
-    operator fun get(i: Int): AminoAcid = sequence[i]
-    override operator fun get(i: IntRange): ProteinSeq = sequence[i]
-    override fun len() : Int = sequence.len()
+                       description: String? = null, annotations: Map<String, String>? = null) :
+SeqRecord(id, name, description, annotations), ProteinSeq by sequence {
 }
