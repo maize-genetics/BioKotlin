ed6a7db09c6bb0ace01713fcd9f4037168615fec
Terry Casstevens <tmc46@cornell.edu>
2020-07-20 19:28:39 -0400

    Changes to deploy BioKotlin to Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,37 +2,43 @@ import org.gradle.api.JavaVersion.VERSION_11
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-//Note Kotlin version nears to be updated in both the buildscript and plugins.
-//Dependencies will follow the buildscript
+// Note Kotlin version needs to be updated in both the buildscript and plugins.
+// Dependencies will follow the buildscript
+
+group = "org.biokotlin"
+version = "1.0"
 
 /*
 This build script is need to use the early access
  */
 buildscript {
-    val kotlinVersion by extra ("1.3.70")
-   // val kotlinVersion by extra ("1.4-M1")
+    val kotlinVersion by extra("1.3.70")
+    // val kotlinVersion by extra ("1.4-M1")
 
     repositories {
         mavenCentral()
         gradlePluginPortal()
-        maven ( "https://dl.bintray.com/kotlin/kotlin-eap" )
+        maven("https://dl.bintray.com/kotlin/kotlin-eap")
     }
 
     dependencies {
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
     }
 }
 
 
 plugins {
     val kotlinVersion = "1.3.70"
-    //val kotlinVersion = "1.4-M1"
+    // val kotlinVersion = "1.4-M1"
     java
     kotlin("jvm") version kotlinVersion
-    //Shadow allows for the creation of fat jars (all dependencies)
+    // Shadow allows for the creation of fat jars (all dependencies)
     id("com.github.johnrengelman.shadow") version "5.2.0"
 
     id("org.jetbrains.dokka") version "0.10.1"
+    `java-library`
+    `maven-publish`
+    signing
 }
 
 repositories {
@@ -40,8 +46,8 @@ repositories {
     jcenter()
     maven("https://maven.imagej.net/content/groups/public/")
     maven("https://jitpack.io")
-    maven ("https://dl.bintray.com/kotlin/kotlin-eap")
-    maven ("https://kotlin.bintray.com/kotlinx")
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+    maven("https://kotlin.bintray.com/kotlinx")
     maven("https://oss.sonatype.org/content/repositories/snapshots/")
 }
 
@@ -50,25 +56,26 @@ dependencies {
 
     implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
     implementation("org.jetbrains.kotlin:kotlin-script-runtime:${kotlinVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
 
 
-    implementation( "org.nield:kotlin-statistics:1.2.1")
+    implementation("org.nield:kotlin-statistics:1.2.1")
     implementation("de.mpicbg.scicomp:krangl:0.11")
 
-    //Biology possible dependencies
-    //Support fasta, bam, sam, vcf, bcf support
+    // Biology possible dependencies
+    // Support fasta, bam, sam, vcf, bcf support
     implementation("com.github.samtools:htsjdk:2.21.3")
 
-    //    implementation("org.jetbrains.bio:bioinf-commons:0.0.9")
+    // implementation("org.jetbrains.bio:bioinf-commons:0.0.9")
 
-    //TASSEL provides strength in marker analysis and mapping
-    //This version of tassel bring along a wide range of kotlin stdlib dependencies.  See last paragraph
-    //https://kotlinlang.org/docs/reference/evolution/compatibility-modes.html
-//    implementation("net.maizegenetics:tassel:5.2.60")
+    // TASSEL provides strength in marker analysis and mapping
+    // This version of tassel bring along a wide range of kotlin stdlib dependencies.  See last paragraph
+    // https://kotlinlang.org/docs/reference/evolution/compatibility-modes.html
+    // implementation("net.maizegenetics:tassel:5.2.60")
 
-    //Wide range of sequence tools in Java - API is dated
-//    implementation("org.biojava:biojava:5.3.0")
-//    implementation("org.biojava:biojava-genome:5.3.0")
+    // Wide range of sequence tools in Java - API is dated
+    // implementation("org.biojava:biojava:5.3.0")
+    // implementation("org.biojava:biojava-genome:5.3.0")
 
     implementation("org.graalvm.sdk:graal-sdk:20.0.0")
     implementation("org.apache.commons:commons-csv:1.8")
@@ -90,15 +97,9 @@ dependencies {
 java {
     sourceCompatibility = VERSION_11
     targetCompatibility = VERSION_11
+    withJavadocJar()
+    withSourcesJar()
 }
-
-//kotlin {
-//    sourceSets{
-//        main. += "src/main/kotlin/"
-//        testsrcDirs += src/test/kotlin/'
-//
-//    }
-//}
 
 tasks.withType<KotlinCompile>().configureEach {
     kotlinOptions.jvmTarget = "11"
@@ -122,17 +123,71 @@ tasks.test {
     }
 }
 
-//kotlin {
-//}
-//compileKotlin {
-//    kotlinOptions.jvmTarget = "11"
-//}
-//compileTestKotlin {
-//    kotlinOptions.jvmTarget = "11"
-//}
-//
-//sourceSets {
-//    main.java.srcDirs += 'src/main/kotlin/'
-//    test.java.srcDirs += 'src/test/kotlin/'
-//}
-//
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "biokotlin"
+
+            from(components["java"])
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+
+            repositories {
+                maven {
+                    val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+                    val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+                    url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+                }
+            }
+
+            pom {
+                name.set("BioKotlin")
+                description.set("BioKotlin aims to be a high-performance bioinformatics library that brings the power and speed of compiled programming languages to scripting and big data environments.")
+                url.set("http://www.biokotlin.org/")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        name.set("Ed Buckler")
+                        email.set("esb33@cornell.edu")
+                    }
+                    developer {
+                        name.set("Vaishnavi Gupta")
+                        email.set("vg222@cornell.edu")
+                    }
+                    developer {
+                        name.set("Terry Casstevens")
+                        email.set("tmc46@cornell.edu")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://bitbucket.org:bucklerlab/biokotlin.git")
+                    developerConnection.set("scm:git:ssh://bitbucket.org:bucklerlab/biokotlin.git")
+                    url.set("https://bitbucket.org/bucklerlab/biokotlin/src")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
