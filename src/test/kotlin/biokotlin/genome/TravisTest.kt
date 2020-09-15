ebb119c67bdcee241e7f97503186aa504b5f0108
Travis Wrightsman <traviswrightsman@gmail.com>
2020-09-15 10:19:24 -0400

    Update Travis test code

--- src/test/kotlin/biokotlin/genome/TravisTest.kt
+++ src/test/kotlin/biokotlin/genome/TravisTest.kt
@@ -1,20 +1,31 @@
 package biokotlin.genome
 
+import biokotlin.seq.NucSeq
+
 fun main() {
 
-    /*val gcSame: (NucSeq, NucSeq) -> Boolean = { a, b -> (a.gc() == b.gc()) }
-    val aSeq = Seq("ACGTCCTG")
-    val positivePeaks = RangeSet<Int>.fromBEDFile("peaks.bed")
-    val negativePeaks = rangeSetOf<Int>()
+    /*
+    val gcSame: (NucSeq, NucSeq) -> Boolean = { a, b -> (a.gc() / a.size() == b.gc() / b.size()) }
+    val repeatOverlapSame: (SRange, SRange, SRange) -> Boolean = { a, b, r -> (a.overlapPerc(r) == b.overlapPerc(r)) }
+    val compFunc: (SRange, SRange, SRange) -> Boolean = {
+        a, b, r -> gcSame(a.seq(), b.seq()) and repeatOverlapSame(a, b, r) }
+
+    val aSeq = NucSeq("ACGTCCTG")
+    val positivePeaks: SRangeSet = bedfileToSRangeSet("peaks.bed")
+    var negativePeaks = overlappingSetOf(SeqPositionRangeComparator.sprComparator, listOf())
 
     for (positivePeak in positivePeaks) {
         val searchSpace = positivePeak
-                .intervalAround(30000)
+                .flankBoth(30000)
+                .toSet()
                 .subtract(
                         positivePeaks.union(negativePeaks)
                 )
-        val pairedInterval = aSeq.pairedInterval(positivePeak, searchSpace, gcSame)
-        negativePeaks.append(pairedInterval)
-    }*/
 
+        val pairedIntervals = aSeq.pairedIntervals(positivePeak, searchSpace, gcSame, n = 5)
+        negativePeaks = negativePeaks.union(pairedIntervals)
+    }
+
+    negativePeaks.toBedFile("peaks.neg.bed")
+     */
 }
