2b4153b0cf95f97f5d21f586d94aeb6cf37cfd62
Terry Casstevens <tmc46@cornell.edu>
2020-09-15 15:40:36 -0400

    Updates to pfam connection test

--- src/test/kotlin/biokotlin/integration/Sarah_pfamConnectionTest.kt
+++ src/test/kotlin/biokotlin/integration/Sarah_pfamConnectionTest.kt
@@ -1,24 +1,21 @@
 package biokotlin.integration
 
-import biokotlin.kegg.Kegg
 import biokotlin.ncbi.UniProt
-import krangl.print
 import khttp.get
-import org.json.XML
-import org.mortbay.xml.XmlParser
-import org.w3c.dom.Element
-import org.w3c.dom.Node
 import org.xml.sax.InputSource
 import java.io.StringReader
 import javax.xml.parsers.DocumentBuilderFactory
 
 
 fun main() {
+
+    setUniProtLogging()
+
     println("We are going from gene name to pathway")
     val proteinRefs = UniProt.dbReferences("O22637")
 
     val payload = mapOf("acc" to "O22637", "output" to "xml") // TODO: what values can be passed here to get the correct protein?
-    val pfamXML = get("http://pfam.xfam.org/protein/protein", params=payload).text
+    val pfamXML = get("http://pfam.xfam.org/protein/protein", params = payload).text
 
     val builderFactory = DocumentBuilderFactory.newInstance()
     val docBuilder = builderFactory.newDocumentBuilder()
@@ -32,23 +29,22 @@ fun main() {
             .nodeValue
     println(sequence)
 
-    val matchList = doc.getElementsByTagName("matches")
-    val match = matchList.item(0)
-    if (match != null) {
-        if (match.hasChildNodes()) {
-            val child = match.firstChild
-            while (child != null) {
-                if (child.nodeType == Node.TEXT_NODE) {
-                    println(child.nodeValue)
-                }
+    val matchList = doc.getElementsByTagName("match")
+
+    for (i in 0 until matchList.length) {
+        val item = matchList.item(i)
+        println(item.attributes.getNamedItem("accession"))
+        val children = item.childNodes
+        for (j in 0 until children.length) {
+            val child = children.item(j)
+            child.attributes?.let {
+                val start = it.getNamedItem("start").nodeValue
+                val end = it.getNamedItem("end").nodeValue
+                println("start: $start")
+                println("end: $end")
+                println(sequence.substring(start.toInt(), end.toInt()))
             }
         }
     }
-
-
-//    println(seqStart)
-//    println(pfamXML)
-
-
 
 }
