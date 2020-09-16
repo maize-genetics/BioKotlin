45def8a965644c7580800ba5447d2c5d354465c5
Terry Casstevens <tmc46@cornell.edu>
2020-09-16 16:04:39 -0400

    Updated PFAM connection to use ebi instead of pfam.  Also switched from XML to JSON

--- src/test/kotlin/biokotlin/integration/PFAM.kt
+++ src/test/kotlin/biokotlin/integration/PFAM.kt
@@ -1,13 +1,14 @@
 package biokotlin.integration
 
 import khttp.get
+import khttp.post
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.jsonArray
+import kotlinx.serialization.json.jsonObject
 import org.ehcache.Cache
 import org.ehcache.config.builders.CacheConfigurationBuilder
 import org.ehcache.config.builders.CacheManagerBuilder
 import org.ehcache.config.builders.ResourcePoolsBuilder
-import org.xml.sax.InputSource
-import java.io.StringReader
-import javax.xml.parsers.DocumentBuilderFactory
 
 private val cacheManager by lazy {
     CacheManagerBuilder.newCacheManagerBuilder().build(true)
@@ -21,50 +22,47 @@ private val proteinCache: Cache<String, PFAMProtein> by lazy {
 
 data class PFAMProtein(val accession: String, val sequence: String, val domains: List<PFAMDomain>)
 
-data class PFAMDomain(val accession: String, val id: String, val type: String, val start: Int, val end: Int)
+data class PFAMDomain(val accession: String, val name: String, val start: Int, val end: Int)
 
 fun protein(accession: String): PFAMProtein {
     return proteinCache[accession] ?: loadProtein(accession)
 }
 
-private fun loadProtein(accession: String): PFAMProtein {
+fun loadProtein(accession: String): PFAMProtein {
 
-    val payload = mapOf("acc" to accession, "output" to "xml")
-    val pfamXML = get("http://pfam.xfam.org/protein/protein", params = payload).text
+    // Use hmmer to search a sequence and get pfam domain information back from it:
+    // https://hmmer-web-docs.readthedocs.io/en/latest/searches.html
+    // Can take either sequence or accession info. It's possible to explicitly set
+    // search parameters, but these have default values that are typically used
+    val baseUrl = "https://www.ebi.ac.uk/Tools/hmmer/search/hmmscan/"
 
-    val builderFactory = DocumentBuilderFactory.newInstance()
-    val docBuilder = builderFactory.newDocumentBuilder()
-    val inputSource = InputSource(StringReader(pfamXML))
-    val doc = docBuilder.parse(inputSource)
+    val post = post(baseUrl, data = mapOf("acc" to accession, "hmmdb" to "pfam")).url
+    val outputType = "?output=json"
+    val resultsUrl = post.plus(outputType)
 
-    val sequence = doc.getElementsByTagName("sequence")
-            .item(0)
-            .firstChild
-            .nodeValue
+    val json = Json.parseToJsonElement(get(resultsUrl).text)
 
-    val domains = mutableListOf<PFAMDomain>()
-    val matchList = doc.getElementsByTagName("match")
-    for (i in 0 until matchList.length) {
-        val item = matchList.item(i)
-        val domainAcc = item.attributes.getNamedItem("accession").nodeValue
-        val domainID = item.attributes.getNamedItem("id").nodeValue
-        val domainType = item.attributes.getNamedItem("type").nodeValue
-        val children = item.childNodes
-        for (j in 0 until children.length) {
-            val child = children.item(j)
-            child.attributes?.let {
-                val start = it.getNamedItem("start").nodeValue.toInt()
-                val end = it.getNamedItem("end").nodeValue.toInt()
-                domains.add(PFAMDomain(domainAcc, domainID, domainType, start, end))
+    assert(json.jsonObject.entries.size == 1)
+    val resultsEntry = json.jsonObject.entries.first()
+    assert(resultsEntry.key == "results")
+    val results = resultsEntry.value.jsonObject
+
+    val hits = results["hits"]?.jsonArray ?: throw IllegalArgumentException("must have hits entry")
+
+    val domains = hits
+            .map { it.jsonObject }
+            .map { hit ->
+                val domainAcc = hit["acc"].toString()
+                val name = hit["name"].toString()
+                val domains = hit["domains"]?.jsonArray ?: throw IllegalArgumentException("must have domains entry")
+                val firstDomain = domains[0].jsonObject
+                val start = firstDomain["ienv"].toString().removeSurrounding("\"").toInt()
+                val end = firstDomain["jenv"].toString().removeSurrounding("\"").toInt()
+                PFAMDomain(domainAcc, name, start, end)
             }
-        }
-    }
+            .toList()
 
-    val result = PFAMProtein(accession, sequence, domains.toList())
-
-    proteinCache.put(accession, result)
-
-    return result
+    return PFAMProtein(accession, "aaaaa", domains)
 
 }
 
