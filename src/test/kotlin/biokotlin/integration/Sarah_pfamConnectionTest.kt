2fe36d6959a9a6e9900c60e3f618f5b202749279
Sarah Jensen <sej65@cornell.edu>
2020-09-16 12:38:41 -0400

    hmmscan on protein sequence

--- src/test/kotlin/biokotlin/integration/Sarah_pfamConnectionTest.kt
+++ src/test/kotlin/biokotlin/integration/Sarah_pfamConnectionTest.kt
@@ -2,6 +2,7 @@ package biokotlin.integration
 
 import biokotlin.ncbi.UniProt
 import khttp.get
+import khttp.post
 import org.xml.sax.InputSource
 import java.io.StringReader
 import javax.xml.parsers.DocumentBuilderFactory
@@ -29,22 +30,39 @@ fun main() {
             .nodeValue
     println(sequence)
 
-    val matchList = doc.getElementsByTagName("match")
+    //Use hmmer to search a sequence and get pfam domain information back from it:
+    // https://hmmer-web-docs.readthedocs.io/en/latest/searches.html
+    // Can take either sequence or accession info. It's possible to explicitly set
+    // search parameters, but these have default values that are typically used
+    val baseUrl = "https://www.ebi.ac.uk/Tools/hmmer/search/hmmscan/"
+    val seq = sequence
+    val acc = "O22637"
+    val hmmdb = "pfam"
 
-    for (i in 0 until matchList.length) {
-        val item = matchList.item(i)
-        println(item.attributes.getNamedItem("accession"))
-        val children = item.childNodes
-        for (j in 0 until children.length) {
-            val child = children.item(j)
-            child.attributes?.let {
-                val start = it.getNamedItem("start").nodeValue
-                val end = it.getNamedItem("end").nodeValue
-                println("start: $start")
-                println("end: $end")
-                println(sequence.substring(start.toInt(), end.toInt()))
-            }
-        }
-    }
+    val post = post(baseUrl, data = mapOf("seq" to seq, "hmmdb" to hmmdb)).url
+    val outputType = "?output=json"
+    val resultsUrl = post.plus(outputType)
+
+    val results = get(resultsUrl)
+    println(results.text)
+
+    
+//    val matchList = doc.getElementsByTagName("match")
+//
+//    for (i in 0 until matchList.length) {
+//        val item = matchList.item(i)
+//        println(item.attributes.getNamedItem("accession"))
+//        val children = item.childNodes
+//        for (j in 0 until children.length) {
+//            val child = children.item(j)
+//            child.attributes?.let {
+//                val start = it.getNamedItem("start").nodeValue
+//                val end = it.getNamedItem("end").nodeValue
+//                println("start: $start")
+//                println("end: $end")
+//                println(sequence.substring(start.toInt(), end.toInt()))
+//            }
+//        }
+//    }
 
 }
