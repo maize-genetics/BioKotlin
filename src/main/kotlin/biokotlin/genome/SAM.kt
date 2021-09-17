c8467f79e9c3a839c286e1f64d43023111b2dc90
Edward Buckler <esb33@cornell.edu>
2021-09-17 17:17:41 -0400

    Using functional literal with receiver
    The order of the data class is not preserved

--- src/main/kotlin/biokotlin/genome/SAM.kt
+++ src/main/kotlin/biokotlin/genome/SAM.kt
@@ -3,7 +3,11 @@ package biokotlin.genome
 import java.io.File
 import htsjdk.samtools.*
 import org.jetbrains.dataframe.*
-import org.jetbrains.dataframe.columns.AnyCol
+import org.jetbrains.dataframe.columns.DataColumn
+import kotlin.reflect.KProperty
+import kotlin.reflect.full.declaredMembers
+import kotlin.reflect.full.withNullability
+import kotlin.reflect.jvm.javaField
 
 data class BKSamRecord(val queryName: String, val queryLength: Int, val strand:String,
                        val targetName: String, val targetStart:Int,val targetEnd: Int, val mapQ:Int,
@@ -12,12 +16,16 @@ data class BKSamRecord(val queryName: String, val queryLength: Int, val strand:S
                        )
 
 
-interface SAMDataFrame<T> : DataFrame<T> {
+class SAMDataFrame(val df: DataFrame<Unit>): DataFrame<Unit> by df {
+    fun myType() ="SAM"
     //Create functions that are specific and useful for SAM dataframe
     //add percent identity column
     //remove unmapped
     //remove secondary mappings
-    
+    fun hi() {
+        println(myType())
+        println(df.print())
+    }
 }
 
 
@@ -33,22 +41,51 @@ fun main() {
     priority.filter { it == "yes" }.print()
     println(df)
 
-    val samDF = convertSAMToDataFrame("/Users/edwardbuckler/Downloads/su1_alignments/" +
-            "Andropogon_virginicus_CanuHiFi-BioNano-Hybrid-scaffolds-merged_v2.fasta_su1.sam", "6" == "5")
-    println(samDF)
+    val samDF = convertSAMToDataFrame("/Users/edbuckler/Downloads/su1_alignments/" +
+            "Andropogon_virginicus_CanuHiFi-BioNano-Hybrid-scaffolds-merged_v2.fasta_su1.sam"
+    ) //{ NM == 5 && numX > 5 }
+    { mapQ >0}
+    //println(samDF)
+    samDF.hi()
+    println(samDF.min("NM"))
+    println(samDF[0])
 }
 
-fun <T> convertSAMToDataFrame(inputFile : String, filter: (T) -> Boolean) : SAMDataFrame<*> {
+fun convertSAMToDataFrame(inputFile : String, filter: BKSamRecord.() -> Boolean) : SAMDataFrame {
     val samReader = loadInSAMReader(inputFile)
 
     val samIterator = samReader.iterator()
     val dataFrameRows = mutableListOf<BKSamRecord>()
     while(samIterator.hasNext()) {
         val currentRecord = samIterator.next()
-        dataFrameRows+= convertSamRecordToDataFrameRow(currentRecord)
+        val bkSamRecord = convertSamRecordToDataFrameRow(currentRecord)
+        if (filter(bkSamRecord)) dataFrameRows.add(bkSamRecord)
+        //dataFrameRows+= convertSamRecordToDataFrameRow(currentRecord)
     }
-    return dataFrameRows.toDataFrameByProperties() as SAMDataFrame<*>
+    return SAMDataFrame(dataFrameRows.toDataFrameByProperties2() as DataFrame<Unit>)
 }
+
+public inline fun <reified T> Iterable<T>.toDataFrameByProperties2(): AnyFrame = T::class.declaredMembers
+
+    .filter { it.parameters.toList().size == 1 }
+    .filter { it is KProperty }
+    .map {
+        val property = (it as KProperty)
+        property.javaField?.isAccessible = true
+        var nullable = false
+        val values = this.map { obj ->
+            if (obj == null) {
+                nullable = true
+                null
+            } else {
+                val value = it.call(obj)
+                if (value == null) nullable = true
+                value
+            }
+        }
+        DataColumn.create(it.name, values, property.returnType.withNullability(nullable))
+    }.let { dataFrameOf(it) }
+
 fun loadInSAMReader(inputFile: String) : SamReader {
     return SamReaderFactory.makeDefault()
         .validationStringency(ValidationStringency.SILENT)
