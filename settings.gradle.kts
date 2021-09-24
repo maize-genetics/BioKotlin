2f3ee4537f056b7011080746471c5775a4735af1
Edward Buckler <esb33@cornell.edu>
2021-09-24 17:27:08 -0400

    Updated dataframe library.  Codegen stubbed out

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,5 +3,9 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
         maven ("https://dl.bintray.com/kotlin/kotlin-eap")
+        //jitpack required for DataFrame Gradle integration
+        //This is used for code generation for DataFrame Schema, however, it does not work
+        //https://github.com/Kotlin/dataframe/tree/eb9ec4fb90f906f6a98e69b9c5a0369009d34bbb/plugins/gradle/codegen
+        maven(url="https://jitpack.io")
     }
 }
