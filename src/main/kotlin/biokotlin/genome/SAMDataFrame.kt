ceb199bbabec60da815924fb9f67293c5bd5ce43
Edward Buckler <esb33@cornell.edu>
2021-09-25 16:59:30 -0400

    Completed tests for SAMDataFrame

--- src/main/kotlin/biokotlin/genome/SAMDataFrame.kt
+++ src/main/kotlin/biokotlin/genome/SAMDataFrame.kt
@@ -4,11 +4,13 @@ import java.io.File
 import htsjdk.samtools.*
 import org.jetbrains.dataframe.*
 import org.jetbrains.dataframe.annotations.DataSchema
+import org.jetbrains.dataframe.columns.DataColumn
 import kotlin.reflect.full.primaryConstructor
 
 @DataSchema
 interface SAMDataFrame {
     val queryName: String
+    val flag: Int
     val queryLength: Int
     val strand: String
     val targetName: String
@@ -26,16 +28,25 @@ interface SAMDataFrame {
     val sequence: String
 }
 
-//The order of this constructor in the data class sets the column order
+/**
+ * A BioKotlin SAM Record, which is the data class used SAMDataFrame
+ */
 data class BKSamRecord(
-    override val queryName: String, override val queryLength: Int, override val strand: String,
+    //The order of this constructor in the data class sets the column order
+    override val queryName: String, override val flag: Int, override val queryLength: Int, override val strand: String,
     override val targetName: String, override val targetStart: Int, override val targetEnd: Int,
     override val mapQ: Int, override val NM: Int, override val numM: Int, override val numEQ: Int,
     override val numX: Int, override val numI: Int, override val numD: Int, override val numH: Int,
     override val numS: Int, override val sequence: String
 ) : SAMDataFrame
 
-//Making the lambda default true makes it option
+/**
+ * Convert a SAM or BAM file and convert to a in memory DataFrame, where the CIGAR strings are parsed into counts.
+ * This can produce a very large file if there is lots of sequences.  To keep things managable, consider
+ * setting includeSequence to false or filter the reads.
+ * e.g. convertSAMToDataFrame("path/myFile.sam",false){NM<2 && numM > 100} - would not include the sequence, and
+ * retain only sequences with less than two mismatches and matched for more than 100 bp.
+ */
 fun convertSAMToDataFrame(
     inputFile: String, includeSequence: Boolean = true,
     filter: BKSamRecord.() -> Boolean = { true }
@@ -59,12 +70,17 @@ fun convertSAMToDataFrame(
     return df.move(*colList).toLeft()
 }
 
-fun DataFrame<SAMDataFrame>.removeUnmapped() {
-    // TODO: 9/18/21
-}
+fun DataFrame<SAMDataFrame>.paired() = this.filter {flag.and(0x1) == 0x1}
+fun DataFrame<SAMDataFrame>.mappedProperPair() = this.filter {flag.and(0x2) == 0x2}
+fun DataFrame<SAMDataFrame>.unmapped() = this.filter { flag.and(0x4) == 0x4 }
+fun DataFrame<SAMDataFrame>.mapped() = this.filter {flag.and(0x4) != 0x4}
+fun DataFrame<SAMDataFrame>.mateUnmapped() = this.filter { flag.and(0x8) == 0x8 }
+fun DataFrame<SAMDataFrame>.mateMapped() = this.filter {flag.and(0x8) != 0x8}
+fun DataFrame<SAMDataFrame>.primaryAlignment() = this.filter {flag.and(0x100) != 0x100}
+fun DataFrame<SAMDataFrame>.notPrimaryAlignment() = this.filter {flag.and(0x100) == 0x100}
 
-fun DataFrame<SAMDataFrame>.removeSecondaryMappings() {}
-fun DataFrame<SAMDataFrame>.propIdentity() {}
+fun DataFrame<SAMDataFrame>.propAlignIdentical() = this.add("pAlignId"){NM.toDouble()/(numM+numD+numI)}
+fun DataFrame<SAMDataFrame>.propAligned() = this.add("pAlign"){numM.toDouble()/queryLength}
 
 internal fun loadInSAMReader(inputFile: String): SamReader {
     return SamReaderFactory.makeDefault()
@@ -75,7 +91,9 @@ internal fun loadInSAMReader(inputFile: String): SamReader {
 internal fun convertSamRecordToDataFrameRow(currentRecord: SAMRecord, includeSequence: Boolean): BKSamRecord {
     val cigarCounts = getCIGARStats(currentRecord.cigar)
     return BKSamRecord(
-        currentRecord.readName, currentRecord.readLength,
+        currentRecord.readName,
+        currentRecord.flags,
+        currentRecord.readLength,
         if (currentRecord.readNegativeStrandFlag) "-" else "+",
         currentRecord?.contig?:"*",
         currentRecord.alignmentStart, currentRecord.alignmentEnd,
@@ -99,15 +117,42 @@ internal fun getCIGARStats(cigar: Cigar): Map<String, Int> {
         .toMap()
 }
 
-fun main() {
-    val samDF = convertSAMToDataFrame(
-        "/Users/edwardbuckler/Downloads/su1_alignments/" +
-                "Andropogon_virginicus_CanuHiFi-BioNano-Hybrid-scaffolds-merged_v2.fasta_su1.sam",
-    false) { NM > 0 }
-    samDF.filter { it[SAMDataFrame::NM] > 0 }
-    println(samDF[0][0])
-    println(samDF.schema())
-    println(samDF)
-}
 
-
+//This is the code generation generated in Jupyter using
+//%trackExecution -all
+//followed by compiling the schema and then replacing cruft
+//
+val DataFrameBase<SAMDataFrame>.flag get() = this["flag"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.flag: Int get() = this["flag"] as Int
+val DataFrameBase<SAMDataFrame>.NM get() = this["NM"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.NM: Int get() = this["NM"] as Int
+val DataFrameBase<SAMDataFrame>.mapQ: DataColumn<Int> get() = this["mapQ"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.mapQ: Int get() = this["mapQ"] as Int
+val DataFrameBase<SAMDataFrame>.numD: DataColumn<Int> get() = this["numD"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.numD: Int get() = this["numD"] as Int
+val DataFrameBase<SAMDataFrame>.numEQ: DataColumn<Int> get() = this["numEQ"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.numEQ: Int get() = this["numEQ"] as Int
+val DataFrameBase<SAMDataFrame>.numH: DataColumn<Int> get() = this["numH"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.numH: Int get() = this["numH"] as Int
+val DataFrameBase<SAMDataFrame>.numI: DataColumn<Int> get() = this["numI"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.numI: Int get() = this["numI"] as Int
+val DataFrameBase<SAMDataFrame>.numM: DataColumn<Int> get() = this["numM"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.numM: Int get() = this["numM"] as Int
+val DataFrameBase<SAMDataFrame>.numS: DataColumn<Int> get() = this["numS"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.numS: Int get() = this["numS"] as Int
+val DataFrameBase<SAMDataFrame>.numX: DataColumn<Int> get() = this["numX"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.numX: Int get() = this["numX"] as Int
+val DataFrameBase<SAMDataFrame>.queryLength: DataColumn<Int> get() = this["queryLength"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.queryLength: Int get() = this["queryLength"] as Int
+val DataFrameBase<SAMDataFrame>.queryName: DataColumn<String> get() = this["queryName"] as DataColumn<String>
+val DataRowBase<SAMDataFrame>.queryName: String get() = this["queryName"] as String
+val DataFrameBase<SAMDataFrame>.sequence: DataColumn<String> get() = this["sequence"] as DataColumn<String>
+val DataRowBase<SAMDataFrame>.sequence: String get() = this["sequence"] as String
+val DataFrameBase<SAMDataFrame>.strand: DataColumn<String> get() = this["strand"] as DataColumn<String>
+val DataRowBase<SAMDataFrame>.strand: String get() = this["strand"] as String
+val DataFrameBase<SAMDataFrame>.targetEnd: DataColumn<Int> get() = this["targetEnd"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.targetEnd: Int get() = this["targetEnd"] as Int
+val DataFrameBase<SAMDataFrame>.targetName: DataColumn<String> get() = this["targetName"] as DataColumn<String>
+val DataRowBase<SAMDataFrame>.targetName: String get() = this["targetName"] as String
+val DataFrameBase<SAMDataFrame>.targetStart: DataColumn<Int> get() = this["targetStart"] as DataColumn<Int>
+val DataRowBase<SAMDataFrame>.targetStart: Int get() = this["targetStart"] as Int
