c3e8b0d471a4ee220f7cdcbaf3c08dc772a1d559
Edward Buckler <esb33@cornell.edu>
2021-09-30 08:06:08 -0400

    Working SAM plotting with overlap module

--- src/main/kotlin/biokotlin/jupyter/PlotlyDataFrame.kt
+++ src/main/kotlin/biokotlin/jupyter/PlotlyDataFrame.kt
@@ -5,39 +5,56 @@ package biokotlin.jupyter
 //generate histogram code
 //generate scatter code
 //generate density code
+//load Gff and combine both in a diagram
+//label the ends with the clipping
 
-import biokotlin.genome.convertSAMToDataFrame
-import biokotlin.genome.targetName
-import biokotlin.genome.targetStart
-import jetbrains.datalore.plot.base.pos.PositionAdjustments
+import biokotlin.genome.*
+import com.google.common.collect.Range
+import com.google.common.collect.RangeSet
+import com.google.common.collect.TreeRangeSet
 import jetbrains.letsPlot.export.ggsave
 import jetbrains.letsPlot.geom.geomSegment
+import jetbrains.letsPlot.geom.geomText
 import jetbrains.letsPlot.ggsize
-import jetbrains.letsPlot.intern.Options
-import jetbrains.letsPlot.intern.PosKind
-import jetbrains.letsPlot.intern.layer.PosOptions
+import jetbrains.letsPlot.label.xlab
 import jetbrains.letsPlot.letsPlot
-import org.jetbrains.dataframe.add
-import org.jetbrains.dataframe.filter
-import org.jetbrains.dataframe.toMap
+import jetbrains.letsPlot.tooltips.layerTooltips
+import org.jetbrains.dataframe.*
+
 
 fun main() {
     val file = "src/test/resources/biokotlin/samBam/SbSam1.sam"
     val samDF = convertSAMToDataFrame(file)
 
-
-    val plotDF = samDF.filter { targetName == "7" && targetStart > 63_000_000 }.add("read"){ index()}
+    val plotDF = samDF.filter { targetName == "7" && targetStart > 63_000_000 }.addYPositionToDF()
 
     println(plotDF)
 
     val p = letsPlot(plotDF.toMap()) { x = "targetStart"; color = "strand" } +
             ggsize(1500, 650) +
-            geomSegment(linetype = 1, size = 5, position = PosOptions(PosKind.JITTER_DODGE, Options.of())) { x = "targetStart"; xend = "targetEnd"; y = "read"; yend = "read" }
-
-            //geomSegment(linetype = 1, size = 5, position = PositionAdjustments.stack()PosOptions(PosKind.STACK, Options.of("vjust" to 10))) { x = "targetStart"; xend = "targetEnd"; y = "read"; yend = "read" }
-    //geomSegment(linetype=1, size=5){x="leftStart"; xend="rightEnd"; y="taxaIndex"; yend="taxaIndex"}
+            xlab("Chromosome ${plotDF.first().targetName}") +
+            geomSegment( linetype = 1, size = 5, speed = 0.5, flow = 0.1, tooltips=layerTooltips("queryName", "targetStart","numM"))
+                { x = "targetStart"; xend = "targetEnd"; y = "yPos"; yend = "yPos" } +
+            geomText(color = "black"){x="targetStart"; y="yPos"; label="startClip"} +
+            geomText(color = "black"){x="targetEnd"; y="yPos"; label="endClip"}
 
     ggsave(p, "Fig1.html")
     ggsave(p, "Fig1dodge.png")
-//println(p)
+}
+
+@Suppress("UnstableApiUsage")
+fun DataFrame<SAMDataFrame>.addYPositionToDF(): DataFrame<SAMDataFrame> {
+    //Create an empty map of Range sets to figure out what is occupied
+    val yPosXRange = sortedMapOf<Int, RangeSet<Int>>()
+    val yPos = mutableListOf<Int>()
+    this.forEach { row ->
+        val range = Range.open(row.targetStart, row.targetEnd)
+        var currY =0
+        //find vertical space
+        while((yPosXRange.get(currY)?.intersects(range) == true)) currY++
+        //adds the range and records the position
+        yPosXRange[currY] =(yPosXRange[currY]?:TreeRangeSet.create()).also { it.add(range) }
+        yPos+=currY
+    }
+    return this.add(yPos.toColumn("yPos"))
 }
