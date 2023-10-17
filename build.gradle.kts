a267ebccf7086f04930f5703c8b1fce4849dc6e2
Terry Casstevens <tmc46@cornell.edu>
2023-10-17 12:05:03 -0400

    New Build Date

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 // Dependencies will follow the buildscript
 
 group = "org.biokotlin"
-version = "0.08"
+version = "0.09"
 
 /*
 This build script is need to use the early access
