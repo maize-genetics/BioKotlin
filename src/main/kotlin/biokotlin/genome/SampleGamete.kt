0c12371062df8f67985429ed8ac3282026f1f9e6
Terry Casstevens <tmc46@cornell.edu>
2024-04-01 23:11:21 -0400

    Adding SampleGamete data class

---
+++ src/main/kotlin/biokotlin/genome/SampleGamete.kt
@@ -0,0 +1,25 @@
+package biokotlin.genome
+
+/**
+ * A SampleGamete is a combination of a sample name and a gamete id.
+ */
+data class SampleGamete(val name: String, val gameteId: Int = 0) : Comparable<SampleGamete> {
+    override fun compareTo(other: SampleGamete): Int {
+        val namesEqual = name.compareTo(other.name)
+        return if (namesEqual == 0) gameteId.compareTo(other.gameteId) else namesEqual
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return if (other is SampleGamete) {
+            name == other.name && gameteId == other.gameteId
+        } else false
+    }
+
+    override fun toString(): String {
+        return "$name:$gameteId"
+    }
+
+    override fun hashCode(): Int {
+        return name.hashCode() + gameteId.hashCode()
+    }
+}
