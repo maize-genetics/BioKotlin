e982096fb3fb9faacdfe3888ab3a187cd5765c93
Terry Casstevens <tmc46@cornell.edu>
2024-04-04 13:50:39 -0400

    Initial unit test for reading VCF file utility

--- src/test/kotlin/biokotlin/util/VCFUtilsTest.kt
+++ src/test/kotlin/biokotlin/util/VCFUtilsTest.kt
@@ -18,6 +18,15 @@ class VCFUtilsTest {
             for (variant in simpleVariants) {
                 val simpleVariant = variant.await()
                 numVariants++
+                if (numVariants == 10) {
+                    assert(simpleVariant.contig == "1") { "Contig is not 1: ${simpleVariant.contig}" }
+                    assert(simpleVariant.start == 144) { "Start is not 144: ${simpleVariant.start}" }
+                    assert(simpleVariant.end == 144) { "End is not 144: ${simpleVariant.end}" }
+                    assert(simpleVariant.refAllele == "A") { "Reference allele is not A: ${simpleVariant.refAllele}" }
+                    assert(simpleVariant.altAlleles.size == 1) { "Number of ALT alleles is not 1: ${simpleVariant.altAlleles.size}" }
+                    assert(simpleVariant.altAlleles[0] == "T") { "ALT allele is not T: ${simpleVariant.altAlleles[0]}" }
+                    assert(simpleVariant.samples.size == 1) { "Number of samples is not 1: ${simpleVariant.samples.size}" }
+                }
             }
         }
 
