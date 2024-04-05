becff288603460da6dec5c4e9292072f77ad2c69
Terry Casstevens <tmc46@cornell.edu>
2024-04-05 09:12:47 -0400

    Added more assert() statements to unit test for VCFUtils

--- src/test/kotlin/biokotlin/util/VCFUtilsTest.kt
+++ src/test/kotlin/biokotlin/util/VCFUtilsTest.kt
@@ -39,6 +39,12 @@ class VCFUtilsTest {
                     assert(simpleVariant.isPhased(0)) { "Genotype is not phased: ${simpleVariant.genotypes[0]}" }
                     assert(simpleVariant.isPhased("LineA")) { "Genotype is not phased: ${simpleVariant.genotypes[0]}" }
                     assert(!simpleVariant.isRefBlock) { "Variant is a reference block" }
+                    assert(simpleVariant.allele(0) == "A") { "Allele is not A: ${simpleVariant.allele(0)}" }
+                    assert(simpleVariant.allele(1) == "T") { "Allele is not T: ${simpleVariant.allele(1)}" }
+                    assert(!simpleVariant.isSNP()) { "Variant is a SNP" }
+                    assert(!simpleVariant.isIndel()) { "Variant is an indel" }
+                    assert(!simpleVariant.isDEL()) { "Variant is a DEL" }
+                    assert(!simpleVariant.isINS()) { "Variant is an INS" }
                 } else if (numVariants == 100) {
                     hundredVariant = simpleVariant
                     assert(tenthVariant < hundredVariant) { "Tenth variant is not less than hundredth variant" }
