ac7a90ec6291542d2d7bc2d16349c172afb4ec39
Terry Casstevens <tmc46@cornell.edu>
2024-04-05 14:49:32 -0400

    Added checks for the input parameters to ValidateAndCorrectGVCFs

--- src/main/kotlin/biokotlin/cli/ValidateAndCorrectGVCFs.kt
+++ src/main/kotlin/biokotlin/cli/ValidateAndCorrectGVCFs.kt
@@ -3,6 +3,7 @@ package biokotlin.cli
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import java.io.File
 
 class ValidateAndCorrectGVCFs : CliktCommand(help = "Validate and correct GVCF files") {
 
@@ -14,6 +15,26 @@ class ValidateAndCorrectGVCFs : CliktCommand(help = "Validate and correct GVCF f
 
     val referenceFile by option(help = "Full path to reference fasta file")
         .required()
+
+    init {
+
+        if (!File(inputDir).isDirectory) {
+            throw IllegalArgumentException("Input GVCF directory does not exist: $inputDir")
+        }
+
+        if (!File(outputDir).isDirectory) {
+            throw IllegalArgumentException("Output GVCF directory does not exist: $outputDir")
+        }
+
+        if (File(inputDir).absolutePath == File(outputDir).absolutePath) {
+            throw IllegalArgumentException("Input and output GVCF directories are the same: $inputDir")
+        }
+
+        if (!File(referenceFile).isFile) {
+            throw IllegalArgumentException("Reference FASTA file does not exist: $referenceFile")
+        }
+
+    }
 
     override fun run() {
         TODO()
