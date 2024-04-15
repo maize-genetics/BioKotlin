f0a9d38326cac102c0aeebaa7b4954a3703b1334
Terry Casstevens <tmc46@cornell.edu>
2024-04-15 15:50:16 -0400

    Removed SimpleVariant.positionRange

--- src/main/kotlin/biokotlin/util/VCFUtils.kt
+++ src/main/kotlin/biokotlin/util/VCFUtils.kt
@@ -1,7 +1,6 @@
 package biokotlin.util
 
 import biokotlin.genome.Position
-import biokotlin.genome.PositionRange
 import biokotlin.genome.SampleGamete
 import htsjdk.variant.vcf.*
 import kotlinx.coroutines.*
@@ -23,7 +22,6 @@ data class SimpleVariant(
 ) : Comparable<SimpleVariant> {
 
     val isRefBlock: Boolean
-    val positionRange by lazy { PositionRange(contig, start, end) }
 
     init {
         isRefBlock = (samples.indices).find { sampleIndex ->
