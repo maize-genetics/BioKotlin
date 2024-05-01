1fc19b853ea638714119b437a5c3624a3146a89b
Terry Casstevens <tmc46@cornell.edu>
2024-05-01 17:28:25 -0400

    Added GetVCFVariantsExample to show how to use the GetVCFVariants utility

---
+++ src/main/kotlin/biokotlin/cli/GetVCFVariantsExample.kt
@@ -0,0 +1,78 @@
+package biokotlin.cli
+
+import biokotlin.genome.Position
+import biokotlin.util.GetVCFVariants
+import biokotlin.util.SimpleVariant
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import org.apache.logging.log4j.LogManager
+
+/**
+ * This is an example use of GetVCFVariants.
+ * See MergeGVCFs.kt for a similar example.
+ */
+class GetVCFVariantsExample : CliktCommand(help = "Example use of GetVCFVariants.") {
+
+    private val myLogger = LogManager.getLogger(GetVCFVariantsExample::class.java)
+
+    val inputDir by option(help = "Full path to input VCF file directory")
+        .required()
+
+    // I put Any?, but you should replace it with the actual type of the result
+    private val resultChannel = Channel<Deferred<Any?>>(100)
+
+    override fun run() {
+
+        // Use debug = true to store the original VCF lines in the SimpleVariant objects
+        val getVCFVariants = GetVCFVariants(inputDir, debug = false)
+
+        // List of lists of samples
+        val samples = getVCFVariants.samples
+
+        val positionsChannel = Channel<Deferred<List<Pair<Position, List<SimpleVariant?>>>>>(100)
+        CoroutineScope(Dispatchers.IO).launch {
+            getVCFVariants.forAll(positionsChannel)
+        }
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            for (deferred in positionsChannel) {
+
+                val block = deferred.await()
+                for ((position, variants) in block) {
+                    resultChannel.send(async {
+                        processPosition(position, variants)
+                    })
+                }
+
+            }
+
+            resultChannel.close()
+
+        }
+
+        runBlocking {
+            // Aggregate results from resultChannel
+            aggregateResults()
+        }
+
+    }
+
+    private fun processPosition(position: Position, variants: List<SimpleVariant?>) {
+        TODO()
+    }
+
+    private suspend fun aggregateResults() {
+
+        for (deferred in resultChannel) {
+            val result = deferred.await()
+            // Process results
+            TODO()
+        }
+
+    }
+
+}
