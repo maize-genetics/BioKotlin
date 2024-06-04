2869f0c048d59f0d442b8f1803f24764638df4a4
Terry Casstevens <tmc46@cornell.edu>
2024-06-04 17:12:55 -0400

    Refactored ValidateVCFUtils for better performance

--- src/main/kotlin/biokotlin/cli/ValidateVCFs.kt
+++ src/main/kotlin/biokotlin/cli/ValidateVCFs.kt
@@ -4,6 +4,7 @@ import biokotlin.util.validateVCFs
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
+import kotlinx.coroutines.runBlocking
 import org.apache.logging.log4j.LogManager
 
 class ValidateVCFs : CliktCommand(help = "Validate (G)VCFs in a directory.") {
@@ -14,10 +15,12 @@ class ValidateVCFs : CliktCommand(help = "Validate (G)VCFs in a directory.") {
         .required()
 
     override fun run() {
-        if (validateVCFs(inputDir).valid) {
-            myLogger.info("All (G)VCF files in $inputDir are valid.")
-        } else {
-            myLogger.error("Some (G)VCF files in $inputDir are invalid.")
+        runBlocking {
+            if (validateVCFs(inputDir).valid) {
+                myLogger.info("All (G)VCF files in $inputDir are valid.")
+            } else {
+                myLogger.error("Some (G)VCF files in $inputDir are invalid.")
+            }
         }
     }
 
