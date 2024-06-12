8f04b4e459115ab0d9a7ceef9eb324c6a289982d
Terry Casstevens <tmc46@cornell.edu>
2024-06-12 11:11:18 -0400

    Added another unit test for MergeGVCFs to test handling of insertions with diploid data

--- src/test/kotlin/biokotlin/cli/MergeGVCFsTest.kt
+++ src/test/kotlin/biokotlin/cli/MergeGVCFsTest.kt
@@ -20,10 +20,13 @@ class MergeGVCFsTest {
         private val userHome = System.getProperty("user.home").replace('\\', '/')
         private val tempDir = "$userHome/temp/biokotlinTests/tempDir/"
         val testMergedOutputFile = "${tempDir}merge_gvcfs_output.vcf"
+        val testMergedDiploidOutputFile = "${tempDir}merge_diploid_gvcfs_output.vcf"
 
         const val gvcfInputDir = "data/test/merge_gvcfs_input/"
+        const val gvcfDiploidInputDir = "data/test/merge_gvcfs_diploid_input/"
 
         const val expectedMergedOutputFile = "data/test/merge_gvcfs_output.vcf"
+        const val expectedMergedDiploidOutputFile = "data/test/merge_diploid_gvcfs_output.vcf"
 
         @JvmStatic
         @BeforeAll
@@ -63,6 +66,31 @@ class MergeGVCFsTest {
         myLogger.info("testMergeGVCFs.vcf actual checksum2: $checksum2")
 
         Assertions.assertEquals(checksum1, checksum2, "testMergeGVCFs.vcf checksums do not match")
+
+    }
+
+    @Test
+    fun testMergeGVCFwithINS() {
+
+        // ./biokotlin-0.16/bin/biokotlin merge-gvcfs
+        // --input-dir /Users/tmc46/git/biokotlin/data/test/merge_gvcfs_input/
+        // --output-file /Users/tmc46/git/biokotlin/data/test/merge_gvcfs_output.vcf
+
+        val result = MergeGVCFs().test(
+            "--input-dir $gvcfDiploidInputDir --output-file $testMergedDiploidOutputFile"
+        )
+
+        myLogger.info("testMergeGVCFwithINS: result output: ${result.output}")
+
+        Assertions.assertEquals(result.statusCode, 0, "status code not 0: ${result.statusCode}")
+
+        var checksum1 = getChecksum(expectedMergedDiploidOutputFile)
+        var checksum2 = getChecksum(testMergedDiploidOutputFile)
+
+        myLogger.info("testMergeGVCFwithINS.vcf expected checksum1: $checksum1")
+        myLogger.info("testMergeGVCFwithINS.vcf actual checksum2: $checksum2")
+
+        Assertions.assertEquals(checksum1, checksum2, "testMergeGVCFwithINS.vcf checksums do not match")
 
     }
 
