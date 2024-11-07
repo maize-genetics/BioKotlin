8c16912ca007bda4694b39e2b8a70c00aca5d0e1
Terry Casstevens <tmc46@cornell.edu>
2024-11-07 15:24:05 -0500

    Added BedUtils.readBedfile()

---
+++ src/main/kotlin/biokotlin/util/BedUtils.kt
@@ -0,0 +1,23 @@
+package biokotlin.util
+
+import biokotlin.genome.PositionRange
+import java.io.File
+import java.util.*
+
+object BedUtils {
+
+    fun readBedfile(bedFileName: String): SortedSet<PositionRange> {
+
+        require(File(bedFileName).exists()) { "File $bedFileName does not exist." }
+
+        return bufferedReader(bedFileName).readLines().map { line ->
+            val lineSplit = line.split("\t")
+            val chrom = lineSplit[0]
+            val start = lineSplit[1].toInt() + 1
+            val end = lineSplit[2].toInt()
+            PositionRange(chrom, start, end)
+        }.toSortedSet()
+
+    }
+
+}
